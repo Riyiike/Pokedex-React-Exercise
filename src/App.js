3397ee46eea3fc61fba7d246241bf6f69ddc249f
Riyiike <odusanyaiyanuoluwa@gmail.com>
2020-05-24 23:16:29 +0100

    first commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import Pokecard from './Pokecard';
+import './App.css';
+
+function App() {
+  return ( <
+    div className = "App" >
+
+    <
+    /div>
+  );
+}
+
+export default App;
