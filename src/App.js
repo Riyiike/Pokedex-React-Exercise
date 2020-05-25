16291b07b34aa2544616d9645dc5ee10bccb4715
Riyiike <odusanyaiyanuoluwa@gmail.com>
2020-05-25 16:19:31 +0100

    creating the pokecard component

--- src/App.js
+++ src/App.js
@@ -3,19 +3,10 @@ import Pokecard from './Pokecard';
 import './App.css';
 
 function App() {
-  return ( <
-    div className = "App" >
-    <
-    Pokecard id = {
-      4
-    }
-    name = "Charmander"
-    type = "fire"
-    exp = {
-      62
-    }
-    /> < /
-    div >
+  return (
+    <div className="App">
+      <Pokecard id={4} name="Charmander" type="fire" exp={62} />
+    </div>
   );
 }
 
